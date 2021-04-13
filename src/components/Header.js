e37f24755f33da51858b51e185cf0f44dd365288
Haojun Gao <gaohaojun980707@sina.com>
2021-04-13 22:51:48 +0800

    My second journey of react

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Header() {
+  return (
+    <nav>
+      <h1>Hello a third time!</h1>
+      <ul>
+        <li>Things</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Header
